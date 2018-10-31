b3fdeb1436c9a9e8a90808f7a0883a8e782c5cd9
Gabriel Flores <gabrielfloresmonsalve@gmail.com>
2018-10-30 21:48:50 -0300

    Se crea la clase escuela y el metodo timbre

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace escuela
+{
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int anioFundacion;
+
+        public void Timbre()
+        {
+            Console.Beep(2000, 3000);
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var EscuelaObj = new Escuela();
+
+            EscuelaObj.nombre = "Exodus Academy";
+            EscuelaObj.direccion = "Angol, Concepción, Chile";
+            EscuelaObj.anioFundacion = 2018;
+
+            Console.WriteLine("Hora del recreoooo! :)");
+
+            EscuelaObj.Timbre();
+        }
+    }
+}
